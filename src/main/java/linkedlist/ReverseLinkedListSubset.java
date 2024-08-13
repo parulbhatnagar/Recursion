3f8e23f73ff7da71862a3804af4da320112b73d9
ParulBhatnagar <parulbhatnagar@gmail.com>
2024-08-14 05:04:35 +0530

    problem for reverse linked list (subset)

--- src/main/java/linkedlist/ReverseLinkedListSubset.java
+++ src/main/java/linkedlist/ReverseLinkedListSubset.java
@@ -1,7 +1,5 @@
 package linkedlist;
 
-import java.util.List;
-
 /**
  * 92. Reverse Linked List II
  * Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
